3300561f626d3420f470a6d76679c3f2961a3f9d
kt <kt@kts-Mac-mini.local>
2025-03-02 02:16:17 -0500

    Deploy to GitHub Pages

--- next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export",
-  basePath: "/tran-vinh-family-tree",
-  assetPrefix: "/tran-vinh-family-tree/",
-};
-
-export default nextConfig;
--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
-import { defineConfig } from 'next';
-
-export default defineConfig({
-  experimental: {
-    appDir: true,
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+  basePath: "/tran-vinh-family-tree",
+  assetPrefix: "/tran-vinh-family-tree/",
+  images: {
+    unoptimized: true,  // Fixes Next.js image optimization issues
   },
-});
+};
+
+export default nextConfig;
